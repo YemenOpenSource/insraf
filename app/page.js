686d74ae50693afd2c245c03251dd19520c705d3
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2023-02-01 23:44:39 +0300

    nav bar

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
 import tw from "tailwind-styled-components"
+import Image from 'next/image';
+import insarf from '@/public/insarf.svg';
 
 const AppBar = tw.div`flex flex-col items-center bg-white shadow-sm`;
 const Container = tw.header`container sticky top-0 z-40 bg-white`;
 const Title = tw.span`mr-4 sm:mr-0 select-none`;
+const Nav = tw.span`bg-blue-700 w-full h-16`;
 const Head = tw.div`
   ${(p) => p.$border ? 'border-b border-b-slate-200' : 'border-none'}
   ${(p) => p.$between ? 'justify-between' : null}
@@ -13,17 +16,38 @@ const Head = tw.div`
   text-2xl 
   font-bolder
 `;
+const Button = tw.button`
+  ${(p) => p.$active ? 'text-blue-700 border-blue-700 font-bolder mr-4' : 'text-gray-500 border-transparent font-regular'}
+  mb-[-20px] 
+  select-none 
+  inline-flex 
+  h-11 
+  items-center 
+  border-b-[4px]  
+  bg-transparent 
+  px-8 
+  py-2  
+  hover:bg-brand-400 
+  focus:outline-none 
+  focus:ring-0 
+  focus:ring-brand-0 
+  focus:ring-offset-0 
+  text-[17px]
+`;
 
 export default function Home() {
   return (
     <AppBar>
+      <Nav>
+        <Image src={insarf} className="w-[130px] select-none m-4" alt="insarf logo" priority />
+      </Nav>
       <Container>
         <Head $border={true} $between={true}>
-          <Title>المشتركين</Title> 
+          <Title>المشتركين</Title>
         </Head>
         <Head $border={false} $between={false}>
-          <button className="mb-[-20px] select-none inline-flex h-11 items-center border-b-[4px] text-blue-700 border-blue-700 bg-transparent px-8 py-2 font-bolder hover:bg-brand-400 focus:outline-none focus:ring-0 focus:ring-brand-0 focus:ring-offset-0 text-[17px] mr-4">الإحصائيات</button>
-          <button className="mb-[-20px] select-none inline-flex h-11 items-center border-b-[4px] text-gray-500 border-transparent bg-transparent px-8 py-2 font-regular hover:bg-brand-400 focus:outline-none focus:ring-0 focus:ring-brand-0 focus:ring-offset-0 text-[17px]">التصنيفات</button>
+          <Button $active={true}>الإحصائيات</Button>
+          <Button>التصنيفات</Button>
         </Head>
       </Container>
     </AppBar>
