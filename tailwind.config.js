ee7c4eceaa88d7d449c19a0aaf310c92c34a4de5
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2023-02-01 14:35:14 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  important: '#root',
+  theme: {
+    extend: {
+      fontFamily: {
+        bolder: 'var(--font-noto-bold)',
+        regular: 'var(--font-noto-regular)'
+      }
+    },
+  },
+  plugins: [],
+}
