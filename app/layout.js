59d124fd0587f823de043f341094fbba618c59c3
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2023-02-01 15:14:00 +0300

    change

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import "./globals.css";
 // inculde the fonts in app by @next/font
 const fontBold = local({ src: '/fonts/NotoSansArabicBold.woff', variable: '--font-noto-bold' });
 const fontMedium = local({ src: '/fonts/NotoSansArabicMedium.woff', variable: '--font-noto-medium' });
-const fontRegular = local({ src: '/fonts/NotoSansArabicRegular.woff', variable: '--font-noto-regular' });
+// const fontRegular = local({ src: '/fonts/NotoSansArabicRegular.woff', variable: '--font-noto-regular' });
 
 export default function RootLayout({ children }) {
   return (
