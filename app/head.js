4ff7bcc47bbe6aaf694dea5c42a7ad823da85cb8
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2023-02-01 23:13:14 +0300

    head

--- app/head.js
+++ app/head.js
@@ -1,11 +1,11 @@
 export default function Head() {
   return (
     <>
-      <title></title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <title>أنصراف | Insraf</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta
         name="description"
-        content="A playground to explore new Next.js 13 app directory features such as nested layouts, instant loading states, streaming, and component level data fetching."
+        content="insraf the final project from faculty of Engg."
       />
       {/* <link rel="icon" href="/favicon.ico" /> */}
     </>
