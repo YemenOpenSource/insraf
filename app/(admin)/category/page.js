82dfdf2c397130932a3d388f323d1738778dc5cc
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2023-02-03 01:47:46 +0300

    layout page

--- app/(admin)/category/page.js
+++ app/(admin)/category/page.js
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>category</div>
+    <div className="m-auto p-2 sm:p-6 flex flex-col items-center w-screen justify-center">
+      <div className="flex w-full flex-col justify-center mt-3 bg-[#fec63d] shadow-md rounded-sm p-4">
+        <div className="text-[#614c19]">
+          <h1 className="font-bolder">
+  بعض العمليات الدي تسهل عملك:
+          </h1>
+          <li className="mr-4">تصفح الطلاب وفقاً لقسم الطالب</li>
+          <li className="mr-4">عرض جميع تفاصيل الطالب</li>
+          <li className="mr-4">سهولة الوصول لطلاب من خلال البحث</li>
+        </div>
+      </div>
+      <div className="flex w-full flex-col justify-center mt-3 bg-white shadow-md rounded-sm p-4">
+        <div className="h-[140px]">H</div>
+      </div>
+    </div>
   )
 }
--- app/(admin)/statistics/page.js
+++ app/(admin)/statistics/page.js
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>statistics</div>
+    <div className="m-auto p-2 sm:p-6 flex flex-col items-center w-screen justify-center">
+      <div className="flex w-full flex-col justify-center mt-3 bg-blue-700 shadow-md rounded-sm p-4">
+        <div className="text-blue-50">
+          <h1 className="font-bolder">
+            ملخص لقاعدة البيانات وفقاً لبيانات التالية:
+          </h1>
+          <li className="mr-4">عدد الطلاب المضافين</li>
+          <li className="mr-4">متوسط العام لحضور</li>
+          <li className="mr-4">متوسط العام لغياب</li>
+          <li className="mr-4">التصنيفات</li>
+        </div>
+      </div>
+      <div className="flex w-full flex-col justify-center mt-3 bg-white shadow-md rounded-sm p-4">
+        <div className="h-[140px]">
+          table
+        </div>
+      </div>
+    </div>
   )
 }
