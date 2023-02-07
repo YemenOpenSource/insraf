9b8b155e93d122417cca5de9bb9db059458985f7
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2023-02-07 11:05:31 +0300

    study

--- app/(admin)/students/page.js
+++ app/(admin)/students/page.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function page() {
     return (
-        <div className="flow-root bg-white rounded shadow">
-
+        <div className="m-auto p-2 sm:p-6 flex flex-col items-center w-screen justify-center">
+            <div className="flex w-full flex-col justify-center p-4 md:container md:mx-auto md:px-4">
+                <div className="grid grid-cols-1">
+s
+                </div>
+            </div>
         </div>
     )
 }
