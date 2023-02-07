5c4c21fde1a3f4458549c5461e5f2e70da36c074
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2023-02-07 21:24:34 +0300

    student

--- app/(admin)/students/page.js
+++ app/(admin)/students/page.js
@@ -1,13 +1,23 @@
-"use client"
-import { Table, HeadTable, RowTable } from "@/ui/Table";
 
 export default function page() {
-
     return (
         <div className="m-auto p-2 sm:p-6 flex flex-col items-center w-screen justify-center">
             <div className="flex w-full flex-col justify-center p-4">
                 <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
+                    {/* Alert */}
+                    <div className="p-4 mb-4 text-[#614c19] rounded-lg bg-[#fec63d]">
+                        <div className="flex items-center">
+                            <h3 className="text-lg font-medium">عرض جميع الطلاب</h3>
+                        </div>
+                        <div className="mt-2 mb-4 text-sm">
+                           عرض جميع الطلاب الموجودين في قاعدة البيانات لجميع المستويات, لتصفح الطلاب وفقاً
+                            للإقسام يمكنك الإنتقال لصفحة التصنيفات او الضغط على 
+                           القسم الموجود في الجدول
+                        </div>
+                    </div>
+                    {/* Alert */}
                     <div className="bg-white relative shadow-md sm:rounded-lg overflow-hidden">
+                        {/* section */}
                         <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                             <div className="w-full md:w-1/2">
                                 <form className="flex items-center">
@@ -36,58 +46,52 @@ export default function page() {
                                         <th scope="col" className="px-4 py-3">اسم الطالب</th>
                                         <th scope="col" className="px-4 py-3">رقم القيد</th>
                                         <th scope="col" className="px-4 py-3">القسم</th>
-                                        <th scope="col" className="px-4 py-3">خيارات</th>
+                                        <th scope="col" className="px-4 py-3">عرض</th>
                                     </tr>
                                 </thead>
                                 <tbody>
+                                    {/* Head Start */}
                                     <tr className="border-b text-center">
-                                        <th scope="row" className="px-4 py-3 font-medium text-blue-700 whitespace-nowrap">منصور أحمد منصور حيدرة</th>
+                                        <th scope="row" className="px-4 py-3 font-medium text-blue-700 whitespace-nowrap text-right">منصور أحمد منصور حيدرة</th>
                                         <td className="px-4 py-3">18600</td>
-                                        <td className="px-4 py-3">B5EC</td>
+                                        <td className="px-4 py-3 underline text-blue-700 font-bold cursor-pointer">B5EC</td>
                                         <td className="px-4 py-3">
-                                            <button className="inline-flex items-center p-0.5 text-sm font-medium text-center text-blue-700 hover:text-blue-800 rounded-lg focus:outline-none" type="button">
+                                            <button className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none" type="button">
                                                 <svg className="w-5 h-5" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                                     <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                                 </svg>
                                             </button>
                                         </td>
                                     </tr>
+                                    {/* Head End */}
+                                    {/* Head Start */}
                                     <tr className="border-b text-center">
-                                        <th scope="row" className="px-4 py-3 font-medium text-blue-700 whitespace-nowrap">منصور أحمد منصور حيدرة</th>
+                                        <th scope="row" className="px-4 py-3 font-medium text-blue-700 whitespace-nowrap text-right">اسامة محمد علي أحمد</th>
                                         <td className="px-4 py-3">18600</td>
-                                        <td className="px-4 py-3">B5EC</td>
+                                        <td className="px-4 py-3 underline text-blue-700 font-bold cursor-pointer">B4IT</td>
                                         <td className="px-4 py-3">
-                                            <button className="inline-flex items-center p-0.5 text-sm font-medium text-center text-blue-700 hover:text-blue-800 rounded-lg focus:outline-none" type="button">
+                                            <button className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none" type="button">
                                                 <svg className="w-5 h-5" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                                     <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                                 </svg>
                                             </button>
                                         </td>
                                     </tr>
+                                    {/* Head End */}
+                                    {/* Head Start */}
                                     <tr className="border-b text-center">
-                                        <th scope="row" className="px-4 py-3 font-medium text-blue-700 whitespace-nowrap">منصور أحمد منصور حيدرة</th>
+                                        <th scope="row" className="px-4 py-3 font-medium text-blue-700 whitespace-nowrap text-right">أحمد محمد علي أحمد</th>
                                         <td className="px-4 py-3">18600</td>
-                                        <td className="px-4 py-3">B5EC</td>
+                                        <td className="px-4 py-3 underline text-blue-700 font-bold cursor-pointer">B3E</td>
                                         <td className="px-4 py-3">
-                                            <button className="inline-flex items-center p-0.5 text-sm font-medium text-center text-blue-700 hover:text-blue-800 rounded-lg focus:outline-none" type="button">
+                                            <button className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none" type="button">
                                                 <svg className="w-5 h-5" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                                     <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                                 </svg>
                                             </button>
                                         </td>
                                     </tr>
-                                    <tr className="border-b text-center">
-                                        <th scope="row" className="px-4 py-3 font-medium text-blue-700 whitespace-nowrap">منصور أحمد منصور حيدرة</th>
-                                        <td className="px-4 py-3">18600</td>
-                                        <td className="px-4 py-3">B5EC</td>
-                                        <td className="px-4 py-3">
-                                            <button className="inline-flex items-center p-0.5 text-sm font-medium text-center text-blue-700 hover:text-blue-800 rounded-lg focus:outline-none" type="button">
-                                                <svg className="w-5 h-5" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                                                </svg>
-                                            </button>
-                                        </td>
-                                    </tr>
+                                    {/* Head End */}
                                 </tbody>
                             </table>
                         </div>
