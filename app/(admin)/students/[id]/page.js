c48e44a1b9685e499f6904e4ad58b7b06688fc14
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2023-02-09 11:47:24 +0300

    card

--- app/(admin)/students/[id]/page.js
+++ app/(admin)/students/[id]/page.js
@@ -7,29 +7,34 @@ import insarf_blue from "@/public/insarf_blue.svg";
 
 export default function page({ params }) {
     return (
-        <div className="m-auto p-2 sm:p-4 flex flex-col items-center w-screen justify-center">
+        <div className="m-auto p-2 sm:p-4 flex flex-col items-center lg:w-2/4 w-screen justify-center">
             <div className="flex w-full flex-col justify-center px-0 sm:px-4">
-                <div className="bg-white shadow-md sm:rounded-lg overflow-hidden p-2 md:py-8 select-none items-center">
-                    <div className="text-right sm:w-2/4 m-auto flex justify-between">
-                        <Image src={insarf_blue} className="w-[160px] select-none" alt="insarf logo" priority />
-                        <Image src={qr} className="w-[100px] select-none" alt="qr" priority />
+                <div className="bg-white rounded-lg shadow-lg border-blue-700 border-2">
+                    <div className="flex items-center p-6 border-b border-blue-700 justify-between">
+                        <div className="flex-shrink-0">
+                            <Image src={insarf_blue} className="w-[160px] select-none" alt="insarf logo" priority />
+                        </div>
+                        <div className="md:ml-6">
+                            <Image src={qr} className="w-[80px] h-[80px] select-none" alt="qr" priority />
+                        </div>
                     </div>
-                    <div className="text-right sm:w-2/4 m-auto my-5">
-                        <p className="uppercase font-medium w-full h-10 bg-[#f3f3f3] text-[#7d7d7d] leading-2 p-2">الاسم :
-                            <span className="text-[#7d7d7d]"> منصور أحمد منصور</span>
+                    <div className="sm:p-6 p-4 select-none">
+                        <Image src={avater} className="w-[150px] select-none m-auto" alt="avater" priority />
+                        <div dir="rtl" className="text-right mb-8">
+                        <p className="uppercase font-bolder w-full h-10 bg-blue-700 text-blue-50 leading-2 p-2">الاسم : منصور أحمد منصور</p>
+                        <p className="uppercase w-full font-bolder h-10 bg-[#fff] text-blue-700 leading-2 p-2">رقم القيد :
+                            <span className="text-red-500 font-bolder"> 18600 </span>
                         </p>
-                        <p className="uppercase font-medium w-full h-10 bg-[#fff] text-[#7d7d7d] leading-2 p-2">رقم القيد :
-                            <span className="text-red-500"> 18600 </span>
+                        <p className="uppercase w-full font-bolder h-10 bg-blue-700 text-blue-50  leading-2 p-2">القسم العلمي :
+                            <span className="text-blue-50 font-bolder"> الكترونية وإتصالات</span>
                         </p>
-                        <p className="uppercase font-medium w-full h-10 bg-[#f3f3f3] text-[#7d7d7d] leading-2 p-2">القسم العلمي :
-                            <span className="text-[#7d7d7d]"> هندسة الكترونية وإتصالات </span>
+                        <p className="uppercase w-full font-bolder h-10 bg-[#fff] text-blue-700 leading-2 p-2">المستوى الدراسي :
+                            <span className="text-blue-700 font-bolder"> الخامس </span>
                         </p>
-                        <p className="uppercase font-medium w-full h-10 bg-[#fff] text-[#7d7d7d] leading-2 p-2">المستوى الدراسي :
-                            <span className="text-[#7d7d7d]"> الخامس </span>
+                        <p className="uppercase w-full font-bolder h-10 bg-blue-700 text-blue-50 leading-2 p-2">الشعبة  :
+                            <span className="text-blue-50 font-medium font-sans"> B5EC </span>
                         </p>
-                        <p className="uppercase font-medium w-full h-10 bg-[#f3f3f3] text-[#7d7d7d] leading-2 p-2">الشعبة  :
-                            <span className="text-[#7d7d7d]"> B5EC </span>
-                        </p>
+                    </div>
                     </div>
                 </div>
             </div>
@@ -47,7 +52,7 @@ export default function page({ params }) {
                 </div>
                 <div className="flex justify-center font-medium flex-col md:flex-row items-center">
                     <div dir="rtl" className="text-right md:ml-8">
-                        <p className="uppercase font-medium w-80 h-10 bg-[#f3f3f3] text-[#7d7d7d] leading-2 p-2">الاسم : منصور أحمد منصور</p>
+                        <p className="uppercase font-medium w-80 h-10 bg-[#f3f3f3] text-white leading-2 p-2">الاسم : منصور أحمد منصور</p>
                         <p className="uppercase font-medium w-80 h-10 bg-[#fff] text-[#7d7d7d] leading-2 p-2">رقم القيد :
                             <span className="text-red-500"> 18600 </span>
                         </p>
