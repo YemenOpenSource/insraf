6873ae40e431ae6ffe462d59a4b48452e8fa96f6
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2023-02-12 20:16:47 +0300

    user

--- components/HeadStudent.js
+++ components/HeadStudent.js
@@ -1,16 +1,26 @@
-import Image from "next/image";
-import AddUser from "@/public/icons/AddUser.svg";
+"use client";
+import { Delete, Edit } from "react-iconly";
 
 export default function HeadStudent() {
     return (
         <div className="flex w-full flex-col justify-center px-0 sm:px-4 mb-4">
             <div className="bg-white rounded-lg shadow-lg h-20 flex justify-evenly items-center select-none">
-                <button className="bg-blue-700 text-sm p-3 rounded-sm text-white ml-4 sm:ml-0 h-10 font-bolder">
-                    <Image src={AddUser} className="w-5 select-none inline-block mx-1 fill-white text-white" alt="insarf logo" priority />
+                <button className="bg-blue-700 hover:bg-blue-600 selection:bg-blue-50 selection:text-blue-900 text-sm p-3 rounded-sm text-white sm:ml-0 font-bolder flex justify-center items-center">
+                    <Edit
+                        style={{ marginLeft: 6 }}
+                        primaryColor='white'
+                        stroke='bold'
+                        size="medium"
+                    />
                     تعديل
                 </button>
-                <button className="bg-red-500 text-sm p-3 rounded-sm text-white ml-4 sm:ml-0 h-10 font-bolder">
-                    <Image src={AddUser} className="w-5 select-none inline-block mx-1 fill-white text-white" alt="insarf logo" priority />
+                <button className="bg-red-600 outline-none hover:bg-red-500 selection:bg-red-50 selection:text-red-900 text-sm p-3 rounded-sm text-white sm:ml-0 font-bolder flex justify-center items-center">
+                    <Delete
+                        style={{ marginLeft: 6 }}
+                        primaryColor='white'
+                        stroke='bold'
+                        size="medium"
+                    />
                     حذف
                 </button>
             </div>
