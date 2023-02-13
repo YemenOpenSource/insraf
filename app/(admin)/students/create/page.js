629a6723e8e0f83b669a9d0df3b39c33607fb309
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2023-02-13 12:40:01 +0300

    Setting

--- app/(admin)/students/create/page.js
+++ app/(admin)/students/create/page.js
@@ -79,11 +79,12 @@ export default function page() {
                     <div className="md:col-start-10 md:col-end-13 order-1 md:order-2">
                         <div className="flex justify-center relative bg-white rounded shadow md:col-start-10 md:col-end-13 order-1 md:order-2 mb-4">
                             <Image src={avater} className="w-[180px] h-[180px] select-none" alt="qr" priority />
-                            <div className="bg-blue-700 select-none absolute w-full h-full rounded opacity-[0.8] text-white text-center leading-3 flex flex-col justify-center items-center">
+                            <div className="bg-blue-700 select-none absolute w-full h-full rounded opacity-[0.9] text-white text-center leading-3 flex flex-col justify-center items-center font-bolder">
                                 <Setting
+                                    style={{ marginBottom: 6 }}
                                     primaryColor='white'
                                     stroke='bold'
-                                    size="large"
+                                    size="xlarge"
                                 />
                                 الإعدادات
                             </div>
