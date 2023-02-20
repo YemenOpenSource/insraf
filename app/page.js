e3cdefe8449c9a190337d6766f70f8fd94b75742
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2023-02-21 01:29:35 +0300

    next auth

--- app/page.js
+++ app/page.js
@@ -1,7 +1,38 @@
-import React from 'react'
+"use client"
+import { useSession, signIn, signOut } from "next-auth/react"
+import { useState } from "react"
 
-export default function page() {
+export default function Component() {
+  const [username, setUserName] = useState()
+  const [password, setPassword] = useState()
+
+  const { data: session } = useSession()
+
+  const handlerlogin = async (e) => {
+    const data = await signIn('credentials', {
+      redirect: false,
+      username,
+      password
+    })
+    console.log(data)
+  }
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.username} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
-    <div>الإحصائيات</div>
+    <>
+        <div>
+          <input type={"text"} onChange={({ target }) => setUserName(target.value)} />
+          <input type={"text"} onChange={({ target }) => setPassword(target.value)} />
+        </div>
+        <button onClick={() => handlerlogin()}>Sign </button>
+
+    </>
   )
 }
