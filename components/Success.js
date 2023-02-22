c403e0b0bf1a5220be18328a0fbab85de514ddfd
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2023-02-23 01:41:57 +0300

    m-none

--- components/Success.js
+++ components/Success.js
@@ -4,7 +4,7 @@ import tw from "tailwind-styled-components";
  styled-components
 */
 const Container = tw.div`max-w-7xl mx-auto px-4 sm:px-6 lg:px-8`;
-const Main = tw.div`bg-white max-w-3xl mx-auto shadow-sm flex flex-col justify-center items-center p-8 mt-4`;
+const Main = tw.div`bg-white max-w-3xl mx-auto shadow-sm flex flex-col justify-center items-center mt-4`;
 const Button = tw.div`inline-flex font-bolder items-center px-3 py-2 border border-transparent 
 shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 
 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mt-2
