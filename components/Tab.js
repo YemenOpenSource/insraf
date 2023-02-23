b0846fd7d9ecf522a6d20b5b3315e1a6df5a5e22
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2023-02-24 00:03:53 +0300

    welcome

--- components/Tab.js
+++ components/Tab.js
@@ -1,16 +1,16 @@
 "use client";
-import Link from "next/link";    
+import Link from "next/link";
 import { Button } from "@/ui/SubHeader";
 import { useSelectedLayoutSegment } from "next/navigation";
 
 export default function SubHeader({ slug, title }) {
-    
+
     const segment = useSelectedLayoutSegment();
     const isActive = slug === segment;
 
     return (
         <Button $active={isActive}>
-            <Link href={`/${slug}`}>
+            <Link href={`/admin/${typeof slug == 'string' ? slug : ''}`}>
                 {title}
             </Link>
         </Button>
